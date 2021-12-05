6a22cd6220c129a19855f2fcdda151c72d74e4ec
adrbin <35666777+adrbin@users.noreply.github.com>
2021-12-05 20:38:41 +0100

    Day 5

---
+++ 2021/5/puzzle.js
@@ -0,0 +1,93 @@
+"use strict";
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _Board_instances, _Board_drawVerticalLine, _Board_drawHorizontalLine, _Board_drawDiagonalLine;
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_js_1 = require("../../utils.js");
+function part1(input) {
+    const board = new Board();
+    for (const line of input) {
+        const [point1, point2] = line.split('->').map(x => x.trim());
+        const [x1, y1] = point1.split(',').map(x => parseInt(x));
+        const [x2, y2] = point2.split(',').map(x => parseInt(x));
+        board.drawLine(x1, y1, x2, y2, false);
+    }
+    return [...board.board.values()].filter(x => x > 1).length;
+}
+class Board {
+    constructor() {
+        _Board_instances.add(this);
+        this.board = new Map();
+    }
+    drawLine(x1, y1, x2, y2, shouldconsiderDiagonals = true) {
+        if (x1 === x2) {
+            __classPrivateFieldGet(this, _Board_instances, "m", _Board_drawVerticalLine).call(this, x1, y1, y2);
+            return;
+        }
+        if (y1 === y2) {
+            __classPrivateFieldGet(this, _Board_instances, "m", _Board_drawHorizontalLine).call(this, x1, x2, y1);
+            return;
+        }
+        if (!shouldconsiderDiagonals) {
+            return;
+        }
+        if (x1 > x2) {
+            [x1, y1, x2, y2] = [x2, y2, x1, y1];
+        }
+        if (x2 - x1 === Math.abs(y2 - y1)) {
+            __classPrivateFieldGet(this, _Board_instances, "m", _Board_drawDiagonalLine).call(this, x1, y1, x2, y2);
+            return;
+        }
+        1 == 1;
+    }
+}
+_Board_instances = new WeakSet(), _Board_drawVerticalLine = function _Board_drawVerticalLine(x, y1, y2) {
+    if (y1 > y2) {
+        [y1, y2] = [y2, y1];
+    }
+    for (let i = y1; i <= y2; i++) {
+        const address = `${x},${i}`;
+        let count = this.board.get(address);
+        if (count === undefined) {
+            count = 0;
+        }
+        this.board.set(address, count + 1);
+    }
+}, _Board_drawHorizontalLine = function _Board_drawHorizontalLine(x1, x2, y) {
+    if (x1 > x2) {
+        [x1, x2] = [x2, x1];
+    }
+    for (let i = x1; i <= x2; i++) {
+        const address = `${i},${y}`;
+        let count = this.board.get(address);
+        if (count === undefined) {
+            count = 0;
+        }
+        this.board.set(address, count + 1);
+    }
+}, _Board_drawDiagonalLine = function _Board_drawDiagonalLine(x1, y1, x2, y2) {
+    const sign = y1 < y2 ? 1 : -1;
+    for (let i = 0; i <= x2 - x1; i++) {
+        const address = `${x1 + i},${y1 + i * sign}`;
+        let count = this.board.get(address);
+        if (count === undefined) {
+            count = 0;
+        }
+        this.board.set(address, count + 1);
+    }
+};
+function part2(input) {
+    const board = new Board();
+    for (const line of input) {
+        const [point1, point2] = line.split('->').map(x => x.trim());
+        const [x1, y1] = point1.split(',').map(x => parseInt(x));
+        const [x2, y2] = point2.split(',').map(x => parseInt(x));
+        board.drawLine(x1, y1, x2, y2, true);
+    }
+    return [...board.board.values()].filter(x => x > 1).length;
+}
+(0, utils_js_1.runPuzzles)(part1, part2, 2021, 5);
+//# sourceMappingURL=puzzle.js.map
---
+++ 2021/5/puzzle.ts
@@ -0,0 +1,104 @@
+import { runPuzzles } from '../../utils.js';
+
+function part1(input: string[]) {
+  const board = new Board();
+  for (const line of input) {
+    const [point1, point2] = line.split('->').map(x => x.trim());
+    const [x1, y1] = point1.split(',').map(x => parseInt(x));
+    const [x2, y2] = point2.split(',').map(x => parseInt(x));
+    board.drawLine(x1, y1, x2, y2, false);
+  }
+
+  return [...board.board.values()].filter(x => x > 1).length;
+}
+
+class Board {
+  board = new Map<string, number>();
+
+  drawLine(
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    shouldconsiderDiagonals = true,
+  ) {
+    if (x1 === x2) {
+      this.#drawVerticalLine(x1, y1, y2);
+      return;
+    }
+
+    if (y1 === y2) {
+      this.#drawHorizontalLine(x1, x2, y1);
+      return;
+    }
+
+    if (!shouldconsiderDiagonals) {
+      return;
+    }
+
+    if (x1 > x2) {
+      [x1, y1, x2, y2] = [x2, y2, x1, y1];
+    }
+
+    if (x2 - x1 === Math.abs(y2 - y1)) {
+      this.#drawDiagonalLine(x1, y1, x2, y2);
+      return;
+    }
+    1 == 1;
+  }
+
+  #drawVerticalLine(x: number, y1: number, y2: number) {
+    if (y1 > y2) {
+      [y1, y2] = [y2, y1];
+    }
+    for (let i = y1; i <= y2; i++) {
+      const address = `${x},${i}`;
+      let count = this.board.get(address);
+      if (count === undefined) {
+        count = 0;
+      }
+      this.board.set(address, count + 1);
+    }
+  }
+
+  #drawHorizontalLine(x1: number, x2: number, y: number) {
+    if (x1 > x2) {
+      [x1, x2] = [x2, x1];
+    }
+    for (let i = x1; i <= x2; i++) {
+      const address = `${i},${y}`;
+      let count = this.board.get(address);
+      if (count === undefined) {
+        count = 0;
+      }
+      this.board.set(address, count + 1);
+    }
+  }
+
+  #drawDiagonalLine(x1: number, y1: number, x2: number, y2: number) {
+    const sign = y1 < y2 ? 1 : -1;
+
+    for (let i = 0; i <= x2 - x1; i++) {
+      const address = `${x1 + i},${y1 + i * sign}`;
+      let count = this.board.get(address);
+      if (count === undefined) {
+        count = 0;
+      }
+      this.board.set(address, count + 1);
+    }
+  }
+}
+
+function part2(input: string[]) {
+  const board = new Board();
+  for (const line of input) {
+    const [point1, point2] = line.split('->').map(x => x.trim());
+    const [x1, y1] = point1.split(',').map(x => parseInt(x));
+    const [x2, y2] = point2.split(',').map(x => parseInt(x));
+    board.drawLine(x1, y1, x2, y2, true);
+  }
+
+  return [...board.board.values()].filter(x => x > 1).length;
+}
+
+runPuzzles(part1, part2, 2021, 5);
