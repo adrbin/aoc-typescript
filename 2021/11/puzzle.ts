9237480fd60be4afeb1449715c476c9667a1f4cf
adrbin <35666777+adrbin@users.noreply.github.com>
2021-12-11 15:39:00 +0100

    Day 11

---
+++ 2021/11/puzzle.ts
@@ -0,0 +1,109 @@
+import { runPuzzles } from '../../utils.js';
+
+const adjacentVectors = [
+  [0, -1],
+  [0, 1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+];
+
+function part1(input: string[]) {
+  const map = input.map(line => line.split('').map(x => parseInt(x)));
+  let flashesCount = 0;
+  for (let i = 0; i < 100; i++) {
+    flashesCount += step(map);
+  }
+
+  return flashesCount;
+}
+
+function step(map: number[][]) {
+  const flashes = increaseEnergyLevels(map);
+  const flashesCount = flash(flashes, map);
+  resetFlashes(map);
+
+  return flashesCount;
+}
+
+function increaseEnergyLevels(map: number[][]) {
+  const flashes = [];
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      map[i][j]++;
+      if (map[i][j] > 9) {
+        flashes.push([i, j]);
+      }
+    }
+  }
+
+  return flashes;
+}
+
+function flash(flashes: number[][], map: number[][]) {
+  let flashesCount = 0;
+  while (flashes.length > 0) {
+    const [y, x] = flashes.pop()!;
+    if (map[y][x] !== 10) {
+      continue;
+    }
+
+    flashesCount++;
+
+    for (const [dy, dx] of adjacentVectors) {
+      if (
+        map[y + dy] === undefined ||
+        map[y + dy][x + dx] === undefined ||
+        map[y + dy][x + dx] >= 10
+      ) {
+        continue;
+      }
+
+      map[y + dy][x + dx]++;
+
+      if (map[y + dy][x + dx] === 10) {
+        flashes.push([y + dy, x + dx]);
+      }
+    }
+  }
+
+  return flashesCount;
+}
+
+function resetFlashes(map: number[][]) {
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      if (map[i][j] > 9) {
+        map[i][j] = 0;
+      }
+    }
+  }
+}
+
+function part2(input: string[]) {
+  const map = input.map(line => line.split('').map(x => parseInt(x)));
+  let i = 0;
+  while (!isSynchronized(map)) {
+    step(map);
+    i++;
+  }
+
+  return i;
+}
+
+function isSynchronized(map: number[][]) {
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      if (map[i][j] !== 0) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+runPuzzles(part1, part2, 2021, 11);
