a4f4575a21b161fc29d0731e624b543c7509646b
adrbin <35666777+adrbin@users.noreply.github.com>
2021-12-14 22:06:27 +0100

    Day 14

--- 2021/14/puzzle.js
+++ 2021/14/puzzle.js
@@ -2,53 +2,70 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const utils_js_1 = require("../../utils.js");
 function part1(input) {
-    let polymer = input[0].split('');
+    return simulatePolymers(input, 10);
+}
+function part2(input) {
+    return simulatePolymers(input, 40);
+}
+function simulatePolymers(input, stepsNumber) {
+    let polymer = createPolymer(input[0]);
     const rules = createRules(input.slice(1));
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < stepsNumber; i++) {
         polymer = step(polymer, rules);
     }
     const counts = countElements(polymer);
     const sortedCounts = [...counts.entries()].sort(([_, count1], [__, count2]) => count1 - count2);
-    return sortedCounts.at(-1)[1] - sortedCounts.at(0)[1];
+    return sortedCounts[sortedCounts.length - 1][1] - sortedCounts[0][1];
+}
+function createPolymer(line) {
+    const polymer = new Map();
+    line.split('').reduce((prev, cur) => {
+        var _a;
+        const elementCount = (_a = polymer.get(prev + cur)) !== null && _a !== void 0 ? _a : 0;
+        polymer.set(prev + cur, elementCount + 1);
+        return cur;
+    });
+    return polymer;
 }
 function createRules(input) {
     const rules = new Map();
     for (const line of input) {
-        const [element1, element2] = line.split(' -> ');
-        rules.set(element1, element2);
+        const [elementPair, element] = line.split(' -> ');
+        rules.set(elementPair, element);
     }
     return rules;
 }
 function step(polymer, rules) {
-    const newPolymer = [polymer[0]];
-    polymer.reduce((prev, cur) => {
-        const newElement = rules.get(prev + cur);
-        if (newElement !== undefined) {
-            newPolymer.push(newElement);
+    var _a, _b;
+    const newPolymer = new Map();
+    for (const [elementPair, count] of polymer) {
+        const newElement = rules.get(elementPair);
+        if (newElement === undefined) {
+            newPolymer.set(elementPair, count);
+            continue;
         }
-        newPolymer.push(cur);
-        return cur;
-    });
+        const newElement1 = elementPair[0] + newElement;
+        const newElement1Count = (_a = newPolymer.get(newElement1)) !== null && _a !== void 0 ? _a : 0;
+        newPolymer.set(newElement1, newElement1Count + count);
+        const newElement2 = newElement + elementPair[1];
+        const newElement2Count = (_b = newPolymer.get(newElement2)) !== null && _b !== void 0 ? _b : 0;
+        newPolymer.set(newElement2, newElement2Count + count);
+    }
     return newPolymer;
 }
 function countElements(polymer) {
     var _a;
     const counts = new Map();
-    for (const element of polymer) {
-        const count = (_a = counts.get(element)) !== null && _a !== void 0 ? _a : 0;
-        counts.set(element, count + 1);
+    for (const [elementPair, elementPairCount] of polymer) {
+        for (const element of elementPair) {
+            const elementCount = (_a = counts.get(element)) !== null && _a !== void 0 ? _a : 0;
+            counts.set(element, elementCount + elementPairCount);
+        }
+    }
+    for (const [element, count] of counts) {
+        counts.set(element, Math.ceil(count / 2));
     }
     return counts;
-}
-function part2(input) {
-    let polymer = input[0].split('');
-    const rules = createRules(input.slice(1));
-    for (let i = 0; i < 40; i++) {
-        polymer = step(polymer, rules);
-    }
-    const counts = countElements(polymer);
-    const sortedCounts = [...counts.entries()].sort(([_, count1], [__, count2]) => count1 - count2);
-    return sortedCounts.at(-1)[1] - sortedCounts.at(0)[1];
 }
 (0, utils_js_1.runPuzzles)(part1, part2, 2021, 14);
 //# sourceMappingURL=puzzle.js.map
--- 2021/14/puzzle.ts
+++ 2021/14/puzzle.ts
@@ -1,72 +1,88 @@
 import { runPuzzles } from '../../utils.js';
 
 function part1(input: string[]) {
-  let polymer = input[0].split('');
+  return simulatePolymers(input, 10);
+}
+
+function part2(input: string[]) {
+  return simulatePolymers(input, 40);
+}
+
+function simulatePolymers(input: string[], stepsNumber: number) {
+  let polymer = createPolymer(input[0]);
   const rules = createRules(input.slice(1));
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < stepsNumber; i++) {
     polymer = step(polymer, rules);
   }
 
   const counts = countElements(polymer);
+
   const sortedCounts = [...counts.entries()].sort(
     ([_, count1], [__, count2]) => count1 - count2,
   );
 
-  return sortedCounts.at(-1)![1] - sortedCounts.at(0)![1];
+  return sortedCounts[sortedCounts.length - 1][1] - sortedCounts[0][1];
+}
+
+function createPolymer(line: string) {
+  const polymer = new Map<string, number>();
+  line.split('').reduce((prev, cur) => {
+    const elementCount = polymer.get(prev + cur) ?? 0;
+    polymer.set(prev + cur, elementCount + 1);
+
+    return cur;
+  });
+
+  return polymer;
 }
 
 function createRules(input: string[]) {
   const rules = new Map<string, string>();
   for (const line of input) {
-    const [element1, element2] = line.split(' -> ');
-    rules.set(element1, element2);
+    const [elementPair, element] = line.split(' -> ');
+    rules.set(elementPair, element);
   }
 
   return rules;
 }
 
-function step(polymer: string[], rules: Map<string, string>) {
-  const newPolymer: string[] = [polymer[0]];
-  polymer.reduce((prev, cur) => {
-    const newElement = rules.get(prev + cur);
-    if (newElement !== undefined) {
-      newPolymer.push(newElement);
+function step(polymer: Map<string, number>, rules: Map<string, string>) {
+  const newPolymer = new Map<string, number>();
+  for (const [elementPair, count] of polymer) {
+    const newElement = rules.get(elementPair);
+    if (newElement === undefined) {
+      newPolymer.set(elementPair, count);
+      continue;
     }
 
-    newPolymer.push(cur);
+    const newElement1 = elementPair[0] + newElement;
+    const newElement1Count = newPolymer.get(newElement1) ?? 0;
+    newPolymer.set(newElement1, newElement1Count + count);
 
-    return cur;
-  });
+    const newElement2 = newElement + elementPair[1];
+    const newElement2Count = newPolymer.get(newElement2) ?? 0;
+    newPolymer.set(newElement2, newElement2Count + count);
+  }
 
   return newPolymer;
 }
 
-function countElements(polymer: string[]) {
+function countElements(polymer: Map<string, number>) {
   const counts = new Map<string, number>();
 
-  for (const element of polymer) {
-    const count = counts.get(element) ?? 0;
-    counts.set(element, count + 1);
+  for (const [elementPair, elementPairCount] of polymer) {
+    for (const element of elementPair) {
+      const elementCount = counts.get(element) ?? 0;
+      counts.set(element, elementCount + elementPairCount);
+    }
+  }
+
+  for (const [element, count] of counts) {
+    counts.set(element, Math.ceil(count / 2));
   }
 
   return counts;
-}
-
-function part2(input: string[]) {
-  let polymer = input[0].split('');
-  const rules = createRules(input.slice(1));
-
-  for (let i = 0; i < 40; i++) {
-    polymer = step(polymer, rules);
-  }
-
-  const counts = countElements(polymer);
-  const sortedCounts = [...counts.entries()].sort(
-    ([_, count1], [__, count2]) => count1 - count2,
-  );
-
-  return sortedCounts.at(-1)![1] - sortedCounts.at(0)![1];
 }
 
 runPuzzles(part1, part2, 2021, 14);
