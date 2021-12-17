17751a220d7dc00dcea3c5d62f2db891597cc7f8
adrbin <35666777+adrbin@users.noreply.github.com>
2021-12-17 19:04:04 +0100

    Day 17 - first version

---
+++ 2021/17/puzzle.ts
@@ -0,0 +1,112 @@
+import { runPuzzles } from '../../utils.js';
+
+function part1(input: string[]) {
+  const match = input[0].match(
+    /target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/,
+  );
+  const [targetXMin, targetXMax, targetYMin, targetYMax] = match!
+    .slice(1)
+    .map(x => parseInt(x));
+
+  const initialMinX = Math.ceil((Math.sqrt(8 * targetXMin + 1) - 1) / 2);
+  const initialMaxX = Math.floor((Math.sqrt(8 * targetXMax + 1) - 1) / 2);
+
+  let maxY = 0;
+  // for (let initialY = 0; initialY < 100; initialY++) {
+  //   for (let initialX = initialMinX; initialX <= initialMaxX; initialX++) {
+  //     const yAtInitialX = initialY * initialX - (initialX * (initialX - 1)) / 2;
+  //     const fallingDownMin = Math.floor(
+  //       (Math.sqrt(8 * (yAtInitialX - targetYMax) + 1) - 1) / 2,
+  //     );
+  //     const fallingDownMax = Math.floor(
+  //       (Math.sqrt(8 * (yAtInitialX - targetYMin) + 1) - 1) / 2,
+  //     );
+  //     if (
+  //       (yAtInitialX > targetYMin && yAtInitialX < targetYMax) ||
+  //       fallingDownMin !== fallingDownMax
+  //     ) {
+  //       const localMaxY = initialY * initialY - (initialY * (initialY - 1)) / 2;
+  //       if (localMaxY > maxY) {
+  //         maxY = localMaxY;
+  //       }
+  //     }
+  //   }
+  // }
+
+  for (let initialY = 0; initialY < 100; initialY++) {
+    for (let initialX = initialMinX; initialX <= initialMaxX; initialX++) {
+      let x = 0,
+        y = 0,
+        dy = initialY,
+        dx = initialX;
+      let wasTargetHit = false;
+      while (y >= targetYMin) {
+        y += dy--;
+        x += dx;
+
+        if (dx > 0) {
+          dx--;
+        }
+
+        if (
+          x >= targetXMin &&
+          x <= targetXMax &&
+          y >= targetYMin &&
+          y <= targetYMax
+        ) {
+          wasTargetHit = true;
+          break;
+        }
+      }
+
+      if (wasTargetHit) {
+        const localMaxY = initialY * initialY - (initialY * (initialY - 1)) / 2;
+        if (localMaxY > maxY) {
+          maxY = localMaxY;
+        }
+      }
+    }
+  }
+  return maxY;
+}
+
+function part2(input: string[]) {
+  const match = input[0].match(
+    /target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/,
+  );
+  const [targetXMin, targetXMax, targetYMin, targetYMax] = match!
+    .slice(1)
+    .map(x => parseInt(x));
+
+  let count = 0;
+
+  for (let initialY = targetYMin; initialY < 100; initialY++) {
+    for (let initialX = 0; initialX <= targetXMax; initialX++) {
+      let x = 0,
+        y = 0,
+        dy = initialY,
+        dx = initialX;
+      while (y >= targetYMin) {
+        y += dy--;
+        x += dx;
+
+        if (dx > 0) {
+          dx--;
+        }
+
+        if (
+          x >= targetXMin &&
+          x <= targetXMax &&
+          y >= targetYMin &&
+          y <= targetYMax
+        ) {
+          count++;
+          break;
+        }
+      }
+    }
+  }
+  return count;
+}
+
+runPuzzles(part1, part2, 2021, 17);
