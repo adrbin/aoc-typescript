bc0f8b611af6d246ae1e4c090a71a927618d78fd
adrbin <35666777+adrbin@users.noreply.github.com>
2022-01-26 21:55:33 +0100

    Day 24

---
+++ 2021/24/puzzle.js
@@ -0,0 +1,140 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_js_1 = require("../../utils.js");
+var Order;
+(function (Order) {
+    Order[Order["Ascending"] = 0] = "Ascending";
+    Order[Order["Descending"] = 1] = "Descending";
+})(Order || (Order = {}));
+const constants = [
+    {
+        divConst: 1,
+        addConst: 12,
+        addConst2: 9,
+    },
+    {
+        divConst: 1,
+        addConst: 12,
+        addConst2: 4,
+    },
+    {
+        divConst: 1,
+        addConst: 12,
+        addConst2: 2,
+    },
+    {
+        divConst: 26,
+        addConst: -9,
+        addConst2: 5,
+    },
+    {
+        divConst: 26,
+        addConst: -9,
+        addConst2: 1,
+    },
+    {
+        divConst: 1,
+        addConst: 14,
+        addConst2: 6,
+    },
+    {
+        divConst: 1,
+        addConst: 14,
+        addConst2: 11,
+    },
+    {
+        divConst: 26,
+        addConst: -10,
+        addConst2: 15,
+    },
+    {
+        divConst: 1,
+        addConst: 15,
+        addConst2: 7,
+    },
+    {
+        divConst: 26,
+        addConst: -2,
+        addConst2: 12,
+    },
+    {
+        divConst: 1,
+        addConst: 11,
+        addConst2: 15,
+    },
+    {
+        divConst: 26,
+        addConst: -15,
+        addConst2: 9,
+    },
+    {
+        divConst: 26,
+        addConst: -9,
+        addConst2: 12,
+    },
+    {
+        divConst: 26,
+        addConst: -3,
+        addConst2: 12,
+    },
+];
+function part1(input) {
+    var _a;
+    const result = solveWs(Order.Descending);
+    return (_a = result === null || result === void 0 ? void 0 : result.join('')) !== null && _a !== void 0 ? _a : 'Did not found';
+}
+function part2(input) {
+    var _a;
+    const result = solveWs();
+    return (_a = result === null || result === void 0 ? void 0 : result.join('')) !== null && _a !== void 0 ? _a : 'Did not found';
+}
+function solveWs(order = Order.Ascending, z = 0, level = 0, cache = new Map()) {
+    if (z < 0 || z > 100000) {
+        return undefined;
+    }
+    if (cache.size % 1000000 === 0) {
+        console.log(cache.size);
+    }
+    const key = `${z},${level}`;
+    if (cache.has(key)) {
+        return cache.get(key);
+    }
+    if (level === 14) {
+        const result = z === 0 ? [] : undefined;
+        cache.set(key, result);
+        return result;
+    }
+    let result;
+    for (let i = 1; i < 10; i++) {
+        const w = order === Order.Ascending ? i : 10 - i;
+        const newZ = calculateZ(w, z, constants[level]);
+        const nextWs = solveWs(order, newZ, level + 1, cache);
+        if (nextWs !== undefined) {
+            result = [w, ...nextWs];
+            break;
+        }
+    }
+    cache.set(key, result);
+    return result;
+}
+function calculate(w, z, { divConst, addConst, addConst2 }) {
+    let x = z % 26;
+    z = Math.floor(z / divConst);
+    x += addConst;
+    x = x === w ? 0 : 1;
+    let y = x * 25;
+    y++;
+    z = z * y;
+    y = (w + addConst2) * x;
+    z = z + y;
+    return z;
+}
+function calculateZ(w, z, { divConst, addConst, addConst2 }) {
+    const div = Math.floor(z / divConst);
+    if (w === (z % 26) + addConst) {
+        return div;
+    }
+    return 26 * div + w + addConst2;
+}
+(0, utils_js_1.runPuzzles)(part1, part2, 2021, 24);
+//# sourceMappingURL=puzzle.js.map
